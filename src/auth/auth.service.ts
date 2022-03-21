687bd9547b3d73142a7de3f701908d7b2fcacbb0
Zetos Lima <cesar_manoel@hotmail.com>
2022-03-21 00:32:26 -0300

    feat: add signin.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
@@ -37,7 +38,22 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { message: 'I am signed in.' };
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    const { hash: _, ...userWithoutPassword } = user;
+    return userWithoutPassword as User;
   }
 }
