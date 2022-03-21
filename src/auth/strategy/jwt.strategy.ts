978547a5840ab90c8d889e0af9b7be3801915f1f
Zetos Lima <cesar_manoel@hotmail.com>
2022-03-21 01:17:01 -0300

    feat: add jwt strategy.

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: { sub: string; username: string }) {
+    return { userId: payload.sub, username: payload.username };
+  }
+}
