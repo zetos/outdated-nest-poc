dd131071eaf15d5658d413b8aacb74e652a7723d
Zetos Lima <cesar_manoel@hotmail.com>
2022-03-21 02:44:35 -0300

    feat: add teardown logic to prisma.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,12 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { PrismaService } from './../src/prisma/prisma.service';
 import { AppModule } from './../src/app.module';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -16,6 +19,9 @@ describe('App e2e', () => {
     );
 
     await app.init();
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
   });
 
   afterAll(() => {
