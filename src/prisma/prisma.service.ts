71538bf5789f18e6dcdfc4c27241072a6a515599
Zetos Lima <cesar_manoel@hotmail.com>
2022-03-21 00:01:52 -0300

    style: fix linter.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,13 +3,13 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
-        super({
-            datasources: {
-                db: {
-                    url: 'postgresql://postgres:123@localhost:5434/nest?schema=public'
-                }
-            }
-        })
-    }
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:123@localhost:5434/nest?schema=public',
+        },
+      },
+    });
+  }
 }
