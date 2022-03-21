15501ae1583202a20f8166c51d186b9268eecb6d
Zetos Lima <cesar_manoel@hotmail.com>
2022-03-21 01:54:19 -0300

    feat: add custom guard & decorator.

---
+++ src/auth/decorator/getUser.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+
+    return data ? request.user[data] : request.user;
+  },
+);
