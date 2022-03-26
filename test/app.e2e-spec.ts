96e982a1a74d0a7d0b2054582504ede639fd9ab5
Zetos Lima <cesar_manoel@hotmail.com>
2022-03-26 14:54:16 -0300

    test: add get current user e2e.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -102,26 +102,35 @@ describe('App e2e', () => {
           .spec()
           .post('/auth/signin')
           .withBody(dto)
-          .expectStatus(200);
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
       });
     });
   });
 
-  // describe('User', () => {
-  //   describe('Get me', () => {});
+  describe('User', () => {
+    describe('Get me', () => {
+      it('should get current user', async () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({ Authorization: 'Bearer $S{userAt}' })
+          .expectStatus(200);
+      });
+    });
 
-  //   describe('Edit user', () => {});
-  // });
+    describe('Edit user', () => {});
+  });
 
-  // describe('Bookmarks', () => {
-  //   describe('Create bookmark', () => {});
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => {});
 
-  //   describe('Get bookmarks', () => {});
+    describe('Get bookmarks', () => {});
 
-  //   describe('Get bookmark by id', () => {});
+    describe('Get bookmark by id', () => {});
 
-  //   describe('Edit bookmarks', () => {});
+    describe('Edit bookmarks by id', () => {});
 
-  //   describe('Delete bookmarks', () => {});
-  // });
+    describe('Delete bookmarks by id', () => {});
+  });
 });
